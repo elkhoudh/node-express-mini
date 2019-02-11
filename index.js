6a5b248c20bfec6225624776898831d141b0ca46
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-02-11 14:13:36 -0500

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // implement your API here
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.listen(5000, () => console.log("listening on port 5000..."));
